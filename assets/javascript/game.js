555f2f0adeecb735271c7983fceea7c62e525e58
Suha Park <suhapark@Suhas-MacBook-Pro.local>
2017-09-09 16:00:09 -0700

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+var crystalOne = Math.floor(Math.random() * (13 - 1) + 1);
+var crystalTwo = Math.floor(Math.random() * (13 - 1) + 1);
+var crystalThree = Math.floor(Math.random() * (13 - 1) + 1);
+var crystalFour = Math.floor(Math.random() * (13 - 1) + 1);
+
+var totalScore = ""; //stores numbers from the crystals
+var randomNumber = Math.floor(Math.random() * (121 - 19) + 19);
+var 
+
+console.log("Crystal One: " + crystalOne);
+console.log("Crystal Two: " + crystalTwo);
+console.log("Crystal Three: " + crystalThree);
+console.log("Crystal Four: " + crystalFour);
+
+console.log("Random Number: " + randomNumber);
+$("#randomNumber").html("Random Number: " + randomNumber);
+
+$(#img).attr()
+
+
+
+
+//attribute and parseInt crystalValue = 0; to add up the numbers 
+//HTML cant do that. It only prints
+
+
+//$(#img).attr("data-crystalValue", crystal1);
+//sum =
